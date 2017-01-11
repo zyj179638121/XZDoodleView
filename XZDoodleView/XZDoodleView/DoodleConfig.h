36c4d74c2ea6596976dd062cf6e8f1203846efcf
zhaoyongjie <18515381796@163.com>
2017-01-11 16:43:40 +0800

    tag change

--- XZDoodleView/XZDoodleView/DoodleConfig.h
+++ XZDoodleView/XZDoodleView/DoodleConfig.h
@@ -10,10 +10,29 @@
 
 @interface DoodleConfig : NSObject
 
+/**
+ 颜色
+ */
 @property (nonatomic, strong) UIColor *drewColor;
+
+/**
+ 画线的宽度
+ */
 @property (nonatomic, assign) CGFloat lineWidth;
+
+/**
+ 线的两端的样式
+ */
 @property (nonatomic, assign) CGLineCap lineCap;
+
+/**
+ 线转弯的时候的样式
+ */
 @property (nonatomic, assign) CGLineJoin lineJoin;
+
+/**
+ 传入的图片
+ */
 @property (nonatomic, strong) UIImage *image;
 
 + (instancetype)config;
