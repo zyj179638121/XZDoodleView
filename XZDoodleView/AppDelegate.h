74be7d951acaeedc9be16c05c60fb18c7e47b615
zhaoyongjie <18515381796@163.com>
2017-01-11 11:29:08 +0800

    Initial Commit

---
+++ XZDoodleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZDoodleView
+//
+//  Created by MYKJ on 17/1/11.
+//  Copyright © 2017年 zhaoyongjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZDoodleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZDoodleView
+//
+//  Created by MYKJ on 17/1/11.
+//  Copyright © 2017年 zhaoyongjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
